cd207dd19726477ea30e008b5abdac8763513e3b
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-10 20:40:21 +0530

    New Files Created For Skills Circle Animation

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,39 +1,9 @@
 import React from 'react'
-import "../Styles/Skills.css"
-
-
-
 
 function Skills() {
-    return (
-        // <div id='Main_Skill_Div'>
-        //     <div id="Cards_Container">
-        //         <div className='Skill_Card'>Python</div>
-        //         <div className='Skill_Card'>Pytest</div>
-        //         <div className='Skill_Card'>Selenium With Python</div>
-        //         <div className='Skill_Card'>HTML & CSS</div>
-        //         <div className='Skill_Card'>Java Script</div>
-        //         <div className='Skill_Card'>Node Js</div>
-        //         <div className='Skill_Card'>Express Js</div>
-        //         <div className='Skill_Card'>React Js</div>
-        //         <div className='Skill_Card'>My SQL</div>
-        //         <div className='Skill_Card'>Docker</div>
-        //         <div className='Skill_Card'>Git & Github</div>
-        //     </div>
-
-
-        <div class="loader-wrapper">
-  <div class="container">
-    <span></span>
-  </div>
-  <p class="center-text">VP</p>
-</div>
-
-
-
-
-        // </div>
-    )
+  return (
+    <div>Skills</div>
+  )
 }
 
 export default Skills
