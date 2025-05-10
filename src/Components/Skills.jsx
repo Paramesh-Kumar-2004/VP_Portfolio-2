c3e4eb33280196e591f3b2d532c350b3c294b844
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-10 20:49:53 +0530

    Percentage Circle Completed Props Also Added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Skills_Animation from './Skills/Skills_Animation'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div>
+      <Skills_Animation Percentage={"100%"}/>
+    </div>
   )
 }
 
---
+++ src/Components/Skills/Skills_Animation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../../Styles/Skills_Animation.css"
+
+
+function Skills_Animation({ Percentage }) {
+    return (
+        <div className="Main_SkillAnimation">
+            <div className="container">
+                <span></span>
+            </div>
+            <p className="Percentage">{Percentage}</p>
+        </div>
+    )
+}
+
+export default Skills_Animation
