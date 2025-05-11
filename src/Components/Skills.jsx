12e738d1f821bc3ddaebae8640a61668a350a2c8
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-11 20:51:10 +0530

    Card Created For 11 Skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -9,11 +9,92 @@ function Skills() {
     <div id='Main_Skill_Div'>
       <div className='Skill_Card_List'>
         <div className='Skill_Card'></div>
-        <h1 class="Skill_Name">
+        <h2 class="Skill_Name">
           <span class="default-text">Python</span>
           <span class="hover-text">Level</span>
-        </h1> 
+        </h2>
       </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Pytest</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Selenium</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h3 class="Skill_Name">
+          <span class="default-text">HTML & CSS</span>
+          <span class="hover-text">Level</span>
+        </h3>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">React Js</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Node JS</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Express JS</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Mysql</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Mongo DB</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Git & Github</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
+      <div className='Skill_Card_List'>
+        <div className='Skill_Card'></div>
+        <h2 class="Skill_Name">
+          <span class="default-text">Docker</span>
+          <span class="hover-text">Level</span>
+        </h2>
+      </div>
+
     </div>
   )
 }
