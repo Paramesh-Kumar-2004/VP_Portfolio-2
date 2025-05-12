e6caf116b6596d58c54288217473283c83f4c371
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-12 19:39:50 +0530

    Updating The Skill Styles

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -25,7 +25,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='Selenium'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Selenium</span>
           <span class="hover-text">Level</span>
@@ -33,7 +33,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='HTML_CSS'></div>
         <h3 class="Skill_Name">
           <span class="default-text">HTML & CSS</span>
           <span class="hover-text">Level</span>
@@ -41,7 +41,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='React_js'></div>
         <h4 class="Skill_Name">
           <span class="default-text">React Js</span>
           <span class="hover-text">Level</span>
@@ -49,7 +49,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='Node_js'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Node JS</span>
           <span class="hover-text">Level</span>
@@ -57,7 +57,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='Express_Js'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Express JS</span>
           <span class="hover-text">Level</span>
@@ -65,7 +65,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='Mysql'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Mysql</span>
           <span class="hover-text">Level</span>
@@ -73,7 +73,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='MongoDB'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Mongo DB</span>
           <span class="hover-text">Level</span>
@@ -81,7 +81,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='Git_Github'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Git & Github</span>
           <span class="hover-text">Level</span>
@@ -89,7 +89,7 @@ function Skills() {
       </div>
 
       <div className='Skill_Card_List'>
-        <div className='Skill_Card'></div>
+        <div className='Skill_Card' id='Docker'></div>
         <h4 class="Skill_Name">
           <span class="default-text">Docker</span>
           <span class="hover-text">Level</span>
@@ -101,4 +101,3 @@ function Skills() {
 }
 
 export default Skills
-
