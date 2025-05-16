eede4f50bf358c9be06208f521c4356426fd2de8
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-16 12:56:04 +0530

    Updating The Skill Styles

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -8,7 +8,61 @@ function Skills() {
   return (
     <div id='Main_Skill_Div'>
 
-      <Skills_Animation />
+      <h1>Skills</h1>
+
+      <div id='Skills_Card'>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"80%"} />
+          <span><h3>Python</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"60%"} />
+          <span><h3>Pytest & Selenium</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"80%"} />
+          <span><h3>HTML & CSS</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"80%"} />
+          <span><h3>React JS</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"80%"} />
+          <span><h3>Node JS</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"80%"} />
+          <span><h3>Express JS</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"70%"} />
+          <span><h3>Github</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"60%"} />
+          <span><h3>Docker</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"60%"} />
+          <span><h3>MYSQL</h3></span>
+        </div>
+
+        <div className='Cards'>
+          <Skills_Animation Level={"50%"} />
+          <span><h3>Mongo DB</h3></span>
+        </div>
+
+      </div>
 
     </div>
   )
--- src/Components/Skills/Skills_Animation.jsx
+++ src/Components/Skills/Skills_Animation.jsx
@@ -3,13 +3,14 @@ import "../../Styles/Skills.css"
 
 
 
-function Skills_Animation({ Percentage }) {
+function Skills_Animation({ Level }) {
     return (
+
         <div class="social-circle">
             <span class="bubble layer1"></span>
             <span class="bubble layer2"></span>
             <span class="bubble layer3"></span>
-            <p class="circle-text">Intermidiate</p>
+            <p class="circle-text">{Level}</p>
         </div>
 
     )
