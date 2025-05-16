b9cc94f0191420f66b61eeb8ab4498ed9f7e9c45
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-16 13:29:55 +0530

    Updating The Skills Styles

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,20 +5,26 @@ function About() {
   return (
     <div id='Main_About_Div'>
 
+      <h1 id='Title'>ABOUT</h1>
+
       <div id='Contents'>
-        <h1 id='Title'>ABOUT</h1>
-        <h3>
-          &#160;&#160;&#160;&#160;Hi, I’m Paramesh Kumar S – a Computer Science and Engineering graduate and a dedicated Full Stack Developer and Automation Enthusiast. I recently completed my B.E. from Erode Sengunthar Engineering College and am currently exploring opportunities as a fresher in the software development field.
-        </h3>
-        <h3>
-        &#160;&#160;&#160;&#160;I’ve built over 10 projects focused on real-world problems, including web applications, automation tools, and AI-based utilities. My experience includes a Full Stack Developer internship at Expleo Group, where I contributed to a large-scale OTA project within an agile development team. I also completed a Python internship at NovaNectar Services, enhancing my backend and scripting experience.
-        </h3>
-        <h3>
-        &#160;&#160;&#160;&#160;I’m passionate about creating meaningful software, continuously improving my craft, and staying updated with the latest trends in technology. Outside of tech, I enjoy cricket, strategy gaming, and learning new things every day.
-        </h3>
+
+        <div id='Info'>
+          <h3>
+            &#160;&#160;&#160;&#160;Hi, I’m Paramesh Kumar S – a Computer Science and Engineering graduate and a dedicated Full Stack Developer and Automation Enthusiast. I recently completed my B.E. from Erode Sengunthar Engineering College and am currently exploring opportunities as a fresher in the software development field.
+          </h3>
+          <h3>
+            &#160;&#160;&#160;&#160;I’ve built over 10 projects focused on real-world problems, including web applications, automation tools, and AI-based utilities. My experience includes a Full Stack Developer internship at Expleo Group, where I contributed to a large-scale OTA project within an agile development team. I also completed a Python internship at NovaNectar Services, enhancing my backend and scripting experience.
+          </h3>
+          <h3>
+            &#160;&#160;&#160;&#160;I’m passionate about creating meaningful software, continuously improving my craft, and staying updated with the latest trends in technology. Outside of tech, I enjoy cricket, strategy gaming, and learning new things every day.
+          </h3>
+        </div>
+
+        <div id='MyImg'></div>
+
       </div>
 
-      <div id='MyImg'></div>
 
     </div>
   )
