8ba80d6954bf544a8721452ad35b6c9fa48ba016
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-17 19:40:22 +0530

    Updating The Projects.css

--- src/Components/Projects/Projects_Component.jsx
+++ src/Components/Projects/Projects_Component.jsx
@@ -7,6 +7,9 @@ function Projects_Component({ ProjectName, Project_ID, Description }) {
     return (
         <div id='Projects_Component_Main'>
             <div id='Project_IMG'></div>
+            <div id='Project_Summary'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam at fugit aperiam eos iste!
+            </div>
             <div>Projects_Component</div>
         </div>
     )
