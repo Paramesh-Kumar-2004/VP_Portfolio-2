de5bcf0e837cdd092d33b3deaf8991500a47cbab
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-17 19:23:23 +0530

    Updating The Projects_Component

---
+++ src/Components/Projects/Projects_Component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function Projects_Component({ ProjectName, Project_ID, Description }) {
+    return (
+        <div>
+            <div>Projects_Component</div>
+        </div>
+    )
+}
+
+export default Projects_Component
+
+
