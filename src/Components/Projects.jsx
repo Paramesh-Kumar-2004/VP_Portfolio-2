4c91cf172559bb02b1df5679a725be3063a3edd1
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-21 20:27:59 +0530

    Updating The Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -15,33 +15,25 @@ function Projects() {
           Project_Link='https://www.google.com'
         />
 
-        {/* <a href="https://www.google.com" target='b'>
-          <Projects_Component ProjectName='SIL Automation' ID_Name='SIL_Automation'
-            Description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci consequatur consequuntur ex deserunt aut.'
-          />
-        </a> */}
-
+        <Projects_Component ProjectName='SIL Automation' ID_Name='SIL_Automation'
+          Description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci consequatur consequuntur ex deserunt aut.'
+          Project_Link='https://www.google.com'
+        />
 
         <Projects_Component ProjectName='Pyro Patrol' ID_Name='Pyro_Patrol'
           Description='The Pyro Patrol Is A Autonomous Fire Detecting And Extungushing Robot.'
           Project_Link='https://www.google.com'
         />
 
-
-
         <Projects_Component ProjectName='Brain Friendly' ID_Name='Brain_Friendly'
           Description='Brain Friendly Is A E-Learning Website Using VR'
           Project_Link='https://www.google.com'
         />
 
-
-
         <Projects_Component ProjectName='Text Based Game' ID_Name='Text_Based_Game'
           Description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci consequatur consequuntur ex deserunt aut.'
           Project_Link='https://www.google.com'
         />
-
-
 
         <Projects_Component ProjectName='Simple AI Voice Assistant' ID_Name='AI_Voice_Assistant'
           Description='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci consequatur consequuntur ex deserunt aut.'
