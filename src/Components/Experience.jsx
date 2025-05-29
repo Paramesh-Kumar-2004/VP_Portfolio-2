9826cecd016a2ab78d1b58189076b302e39ad9c7
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-29 20:29:43 +0530

    Updating The Experience Styles

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -8,9 +8,12 @@ function Experience() {
         <div id='Main_Experience_Div'>
             <div>
                 <h1 id='Title'>Experience</h1>
-                <div className="Experience_Card">
 
+                <div className="Experience_Cards">
+                    <div className="Experience_Card"></div>
                 </div>
+
+
             </div>
         </div>
     )
