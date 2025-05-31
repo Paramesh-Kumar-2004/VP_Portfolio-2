fa8a438f3b13cfb23019a4cb2614a3d597bebd54
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-05-31 19:43:14 +0530

    Updating The Skill Page

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -13,52 +13,52 @@ function Skills() {
       <div id='Skills_Card'>
 
         <div className='Cards'>
-          <Skills_Animation Level={"Intermidiate"} IdName={"Python"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"Python"} />
           <span><h3>Python</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"60%"} IdName={"Pytest"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"Pytest"} />
           <span><h3>Pytest & Selenium</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"80%"} IdName={"HTML_CSS"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"HTML_CSS"} />
           <span><h3>HTML & CSS</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"70%"} IdName={"Java_Script"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"Java_Script"} />
           <span><h3>Java Script</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"80%"} IdName={"React"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"React"} />
           <span><h3>React JS</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"80%"} IdName={"NodeJs"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"NodeJs"} />
           <span><h3>Node JS</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"80%"} IdName={"ExpressJs"} />
+          <Skills_Animation Level={"Intermediate "} IdName={"ExpressJs"} />
           <span><h3>Express JS</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"Basic"} IdName={"Git_Docker"}/>
+          <Skills_Animation Level={"Basic"} IdName={"Git_Docker"} />
           <span><h3>Git & Docker</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"60%"} IdName={"MYSQL"}/>
+          <Skills_Animation Level={"Basic"} IdName={"MYSQL"} />
           <span><h3>MYSQL</h3></span>
         </div>
 
         <div className='Cards'>
-          <Skills_Animation Level={"50%"} IdName={"MongoDB"} />
+          <Skills_Animation Level={"Basic"} IdName={"MongoDB"} />
           <span><h3>Mongo DB</h3></span>
         </div>
 
