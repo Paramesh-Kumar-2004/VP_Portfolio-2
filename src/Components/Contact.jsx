0fc9965dd48fa26e1ae374017277f547b5750143
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-06-13 15:17:58 +0530

    Modified: Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,19 +7,23 @@ function Contact() {
     <div id='Main_Contact_Div'>
       <h1 id='Title'>Contact</h1>
       <div id="InputFeild">
-        <div >
+        <form action="submit">
 
-          <div>
-            <label htmlFor="name">Name :</label>
-            <input type="text" name="name" id="Name" placeholder='Enter Your Name' />
+          <div >
+
+            <div>
+              <label htmlFor="name">Name :</label>
+              <input type="text" name="name" id="Name" placeholder='Enter Your Name' />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email :</label>
+              <input type="email" name="email" id="Email" placeholder='Enter Your Email' />
+            </div>
+
           </div>
 
-          <div>
-            <label htmlFor="email">Email :</label>
-            <input type="email" name="email" id="Email" placeholder='Enter Your Email' />
-          </div>
-
-        </div>
+        </form>
       </div>
     </div>
   )
