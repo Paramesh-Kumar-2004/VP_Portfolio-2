9047115a34cd91237ac95b45d6cad103d8d415bb
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-06-16 15:19:29 +0530

    Making A Feedback Form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Styles/Contact.css"
 
 
+
 function Contact() {
+
+  const [FormData, setFormData] = useState({
+    UserName: "",
+    UserEmail: "",
+    Message: ""
+  })
+
+  const HandleSubmit = (e) => {
+    alert(e.target.value)
+  }
+
   return (
     <div id='Main_Contact_Div'>
       <h1 id='Title'>Contact</h1>
+      <form onSubmit={HandleSubmit}>
 
+        <div id="name">
+          
+        </div>
+
+
+      </form>
     </div>
   )
 }
