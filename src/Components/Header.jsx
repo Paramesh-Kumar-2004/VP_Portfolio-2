30232c2433a33b03a731c67c9b6e371433286c6c
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-07-01 19:28:51 +0530

    Editing The Header.css (Responsive)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Styles/Header.css"
 
 
 function Header() {
+
+  const [OpenMenu, setOpenMeu] = useState(false)
+  console.log(OpenMenu)
+
+  function ChangeMenu() {
+    setOpenMeu(!OpenMenu)
+  }
+
   return (
     <div id="Main_Header_Div">
       <nav id='nav'>
         <h2 id='name'>Paramesh Kumar S </h2>
-        <ul id='links'>
-          <li id="Menu">#</li>
-          <li className='lists'><a href="#Main_Home_Div">Home</a></li>
-          <li className='lists'><a href="#Main_About_Div">About</a></li>
-          <li className='lists'><a href="#Main_Skill_Div">Skills</a></li>
-          <li className='lists'><a href="#Main_Project_Div">Projects</a></li>
-          <li className='lists'><a href="#Main_Experience_Div">Experience</a></li>
-          <li className='lists'><a href="#Main_Contact_Div">Contact</a></li>
-        </ul>
+        <h3 id="Menu" onClick={ChangeMenu}>#</h3>
+        {OpenMenu && (
+          <ul id='links'>
+            <li className='lists' onClick={ChangeMenu}><a href="#Main_Home_Div">Home</a></li>
+            <li className='lists' onClick={ChangeMenu}><a href="#Main_About_Div">About</a></li>
+            <li className='lists' onClick={ChangeMenu}><a href="#Main_Skill_Div">Skills</a></li>
+            <li className='lists' onClick={ChangeMenu}><a href="#Main_Project_Div">Projects</a></li>
+            <li className='lists' onClick={ChangeMenu}><a href="#Main_Experience_Div">Experience</a></li>
+            <li className='lists' onClick={ChangeMenu}><a href="#Main_Contact_Div">Contact</a></li>
+          </ul>
+        )}
       </nav>
     </div>
   )
